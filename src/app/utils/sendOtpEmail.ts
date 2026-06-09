da818cb97c43171040ef0c680ffa302757eacf1e
khaledssbd <khaledssbd@gmail.com>
2026-06-09 11:59:09 +0600

    vercel prod image updated

--- src/app/utils/sendOtpEmail.ts
+++ src/app/utils/sendOtpEmail.ts
@@ -50,7 +50,7 @@ const sendOtpEmail = async ({
           filename: 'logo.png',
           path:
             config.NODE_ENV === 'production'
-              ? 'https://res.cloudinary.com/dweesppci/image/upload/v1771818383/1771818383275-KHALED-SIDDIQUE.png' // ✅ works on Vercel
+              ? 'https://res.cloudinary.com/dweesppci/image/upload/v1780984105/1780984105497-KHALED-SIDDIQUE.png' // ✅ works on Vercel
               : path.join(__dirname, 'assets', 'logo.png'), // ✅ works locally
           cid: logoCid, // Embed logo with CID
         },
